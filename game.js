fbb632ea6b5a143561acffb214926152d5370405
arnabdas95 <dasarnab1411995@gmail.com>
2020-05-25 09:04:24 +0530

    change distance function add calc distance function add document.location.reload() method for restart game

--- game.js
+++ game.js
@@ -1,24 +1,11 @@
 let canvas = document.querySelector('canvas');
 let play = canvas.getContext("2d");
 
-
 //set canvas size
-//canvas.width = 600;
-//canvas.height = 600;
-canvas.width = window.innerWidth * .4;
-canvas.height = window.innerHeight;
+canvas.width = 600;
+canvas.height = 600;
 
 
-//modify canvas size when window is resized 
-// window.addEventListener("resize", function() {
-//     canvas.width = window.innerWidth *.4;
-//     canvas.height = window.innerHeight;
-
-// });
-
-
-
-run = true;
 
 // initialy bar's attribute 
 let bar_x = Math.floor(canvas.width / 2 - 60);
@@ -26,17 +13,14 @@ let bar_y = Math.floor(canvas.height - canvas.height / 10);
 let bar_w = 120;
 let bar_h = 10;
 let bar_color = "green"
-
+let bar_speed = 40;
 
 //create the  bar that collects the  bullets
 function bar() {
+    play.beginPath();
     play.fillRect(bar_x, bar_y, bar_w, bar_h);
+    play.closePath();
 }
-
-
-
-//define spped of bar 
-let bar_speed = 40;
 
 //funtion for move the bar
 window.addEventListener("keydown", function(event) {
@@ -56,34 +40,52 @@ window.addEventListener("keydown", function(event) {
 
 })
 
-
-
-
-
-//distance
-function distance(x1, y1, x2, y2) {
-
+//function for detecting the collision of ball with bar
+// val is 1 when ball makes a collision with the bar otherwise return zero
+let val;
+function calcDistance(x1, y1, x2, y2) {
+    val = 0;
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
-    return Math.sqrt((Math.pow(this.x1 - this.x2, 2)) + Math.pow(this.y1 - this.y2, 2));
+    low = this.y2;
+    high = this.y2 - bar_h;
+    if ( this.x1>this.x2  &&  this.x1<=this.x2+bar_w) {
+        console.log("entry")
+        if (this.y2 -bar_h <= this.y1 && (this.y1 + bullet_radius) <= this.y2 + bar_h) { val = 1; }
 
+        console.log(val, this.x1, this.x2, this.y1, this.y2);
+        return val;
+    } else { console.log(val, this.x1, this.x2, this.y1, this.y2); return val; }
 
 
 }
 
-
 // function Score() {
+let score=0;
+//Create function to keep track of score
+function Score(){
+    play.font = '20px Arial';
+   // play.fillStyle = 'black';
+  
+    play.fillText("score: ",300,300); //position score at 8,20 on the x,y axis of the canvas
+    console.log('xxxx');
 
-// }
+}
+t = setInterval(function() {
+    
+   
+    Score();
+}, 1000);
 
 
+//
 
 
 
 //create bullets 
-let bullet_radius = 20;
+let bullet_radius = 15;
 let bullet_x = canvas.width / 2;
 let bullet_y = bullet_radius;
 x_direction = 6;
@@ -103,6 +105,7 @@ function Bul(x, y, dx, dy, radius) {
         play.stroke();
         play.fillStyle = color;
         play.fill();
+        play.closePath;
 
 
     }
@@ -123,14 +126,14 @@ var bul = new Bul(bullet_x, bullet_y, x_direction, y_direction, bullet_radius);
 
 //function for the game 
 function move() {
-    if (run == true) {
-        //Score();
+ 
+
+       
         requestAnimationFrame(move);
         play.clearRect(0, 0, canvas.width, canvas.height);
         bul.update();
-
-        dist = distance(bul.x, bul.y, bar_x + 60, bar_y);
-        if (dist < 30) {
+        dist = calcDistance(bul.x, bul.y, bar_x, bar_y);
+        if (dist == 1) {
             bul.dy = -bul.dy;
             play.fillStyle = "yellow";
             play.fill();
@@ -139,13 +142,13 @@ function move() {
         }
         bar();
         if (bul.y + bul.radius > canvas.height) {
-            console.log("hello");
-            run = false;
+
+        
             alert("game over");
+            document.location.reload();
 
         }
     }
 
-}
 
 move();
